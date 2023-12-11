a234525145bb3c8a7237fc94dd3426e92be1fc3e
bpbryalex98 <bpbryalex98@gmail.com>
2023-12-11 13:46:08 -0500

    Paquetes DAO y Domain

--- src/main/java/com/banquito/core/banking/cuenta/service/CuentaIntervinientesService.java
+++
@@ -1,9 +0,0 @@
-package com.banquito.core.banking.cuenta.service;
-
-import java.util.List;
-
-import com.banquito.core.banking.cuenta.domain.CuentaIntervinientes;
-
-public interface CuentaIntervinientesService {
-    List<CuentaIntervinientes> GetAll();
-}
